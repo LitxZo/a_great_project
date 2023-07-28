cf5bd015588904c4e05b98dd6a7e742a2813221d
LitxZo <1044005014@qq.com>
2023-07-28 16:48:24 +0800

    在InitRouter()中添加了context，现在可以优雅退出gin了。

--- router/router.go
+++ router/router.go
@@ -1,7 +1,12 @@
 package router
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
@@ -19,11 +24,18 @@ func RegistRouter(fn FnRegistRouter) { //将需要注册的路由函数放入数
 }
 
 func InitRouter() {
+
+	ctx, cancelCtx := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancelCtx()
+
 	//初始化gin。
 	r := gin.Default()
 
 	//从配置文件中读取port信息和路由根path信息。
 	port := ":" + viper.GetString("Server.port")
+	if port == ":" { //若配置文件没读取到
+		port += "9999"
+	}
 	rootgroup := r.Group(viper.GetString("Server.rootgroup"))
 
 	fmt.Println(viper.GetString("Server.rootgroup"))
@@ -38,8 +50,32 @@ func InitRouter() {
 		fmt.Println(v.Path)
 	}
 
-	//GIN，启动！！
-	r.Run(port)
+	server := &http.Server{
+		Addr:    port,
+		Handler: r,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Println("Start Server Error: ", err.Error())
+			return
+		}
+	}()
+
+	<-ctx.Done()
+
+	ctx, cancelShutDown := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelShutDown()
+
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Println("Server Shutdown Error: ", err.Error())
+	}
+	// //GIN，启动！！
+	// err := r.Run(port)
+	// if err != nil {
+	// 	panic(fmt.Sprintf("Start Server Error: %v", err.Error()))
+	// }
+	fmt.Println("Server ShutDown Success!")
 }
 
 func InitBaseRouters() {
